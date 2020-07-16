42349fce3815fb6d9deb9deacee85715a0fea481
codeindevelopment <codeindevelopment@gamil.com>
2020-07-16 22:08:46 +0430

    add react-router-dom and config routes

---
+++ src/App/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomePage() {
+    return (
+        <div>
+            <h2>Hello React</h2>
+            <h2>سلام ریکت</h2>
+        </div>
+    )
+}
