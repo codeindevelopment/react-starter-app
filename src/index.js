f1e44b64cc7dd52e3a9b2803b2153f335ad068ef
codeindevelopment <codeindevelopment@gamil.com>
2020-07-17 02:22:35 +0430

    add font icons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 import "./App/scss/_EnglishFonts.scss"; // Import English Fonts
 import "./App/scss/_PersianFonts.scss"; // Import Persian Fonts
+import "./App/scss/icons/flaticon/flaticon.css"; //Import FlatIcon1
+import "./App/scss/icons/flaticon2/flaticon.css"; //Import FlatIcon2
+import "./App/scss/icons/blueicon/font/ki.css";   //Import Ki-Bluicon
+import "@fortawesome/fontawesome-free/css/all.min.css" //Import FontAwesome
 
 import "./App/scss/style.scss"; // Global Style
 
