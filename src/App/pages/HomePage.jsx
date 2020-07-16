87b3bb965420d5249877cb1b4b3fb85baed3de77
codeindevelopment <codeindevelopment@gamil.com>
2020-07-17 02:55:41 +0430

    add react-helmet and add meta tags

--- src/App/pages/HomePage.jsx
+++ src/App/pages/HomePage.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
+import { Helmet } from "react-helmet";
 
 export default function HomePage() {
   return (
-    <div>
-      <h2>Hello React</h2>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content="React Starter App" />
+        <title>HomePage</title>
+      </Helmet>
+
+      
 
       <h2>
         <FormattedMessage id="WELCOME.REACT" />
       </h2>
-    </div>
+    </>
   );
 }
